e32f3ab1b8a42c840fc825221bb1d55eaad8bf4b
JesusGMgit <jesus.garcia.m1488@hotmail.com>
2022-10-13 07:12:11 -0500

    cambio en conexion_db_2

--- php/conexion_db_2.php
+++ php/conexion_db_2.php
@@ -3,10 +3,10 @@
 //parametros de conexion
 //$servername = "localhost";
 $servername = "localhost";
-//$username = "u142007641_admin_test";
-$username="root";
+$username = "u142007641_admin_test";
+//$username="root";
 //$password = "SADS@db1988s";
-$password = "Admintest88";
+$password = "Adminsads14";
 $db = "u142007641_sads_db";
 //$db = "sads_db";
 
@@ -14,7 +14,8 @@ $db = "u142007641_sads_db";
 $conn = new mysqli($servername, $username, $password, $db);
 // Check connection
 if ($conn->connect_error) {
-    echo "conexion fallida";
+    
     die("Connection failed: " . $conn->connect_error);
+    echo "conexion fallida";
 }
 ?>
