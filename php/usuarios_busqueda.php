00dfffcae53c137cdb134234722bdf707523130f
JesusGMgit <jesus.garcia.m1488@hotmail.com>
2022-10-18 13:38:52 -0500

    cambio pagina busqueda ventana modal de info

--- php/usuarios_busqueda.php
+++ php/usuarios_busqueda.php
@@ -183,7 +183,7 @@ if(!isset($_SESSION["S_usuario_conectado"]) || $_SESSION["S_usuario_conectado"] 
     <div id="info" class="w3-modal">
         <div class="w3-modal-content w3-card-4 w3-animate-zoom" style="max-width:600px">
             <div class="w3-center"><br>
-                <span onclick="document.getElementById('id01').style.display='none'" class="w3-button w3-xlarge w3-transparent w3-display-topright" title="Close Modal">x</span>
+                <span onclick="document.getElementById('info').style.display='none'" class="w3-button w3-xlarge w3-transparent w3-display-topright" title="Close Modal">x</span>
             </div>
             <br>
             <div class="w3-contanier w3-center">
