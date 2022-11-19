7bc0b20fc40bd89942c1e7aead4f92a775995cdc
JesusGMgit <jesus.garcia.m1488@gmail.com>
2022-11-19 14:06:22 -0600

    checando conexion con db

--- backend/api/clase_usuario.php
+++ backend/api/clase_usuario.php
@@ -3,6 +3,24 @@
 require_once "../conexiones/conexion_db.php";
 
 class Usuario{
+    public static function getAll(){
+        $conexion_db =new Conexion();
+        $query = "SELECT *FROM  usuarios";
+        $resultado = $conexion_db->query($query);
+        $datos = [];
+        if($resultado->num_rows){
+            while($row = $resultado-> fetch_assoc()){
+                $datos[]=[
+                    'Us_ID'=>$row['Us_ID'],
+                    'Us_Usuario'=>$row['Us_Usuario'],
+                    'Us_Nivel'=>$row['Us_Nivel'],
+                    'Us_Contra'=>$row['Us_Contra'],
+                    'Us_Descripcion'=>$row['Us_Descripcion'],
+                ];
+            }//end while
+            return $datos;
+        }
+    }//end getAll
     
 }
 ?>
