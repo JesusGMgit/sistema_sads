ee36f64bb823b686b1831d3885cfce8f4ddd9f49
JesusGMgit <jesus.garcia.m1488@gmail.com>
2022-11-19 14:42:17 -0600

    por id

--- backend/clases/clase_usuario.php
+++ backend/clases/clase_usuario.php
@@ -19,11 +19,26 @@ class Usuario{
                 ];
             }//end while
             return $datos;
-        }
+        }//end if
     }//end getAll
     
     public static function getWhere($id_usuario) {
-        return "id mandada ". $id_usuario . ", pruebas.";
-    }
+        $conexion_db =new Conexion();
+        $query = "SELECT *FROM  usuarios WHERE Us_ID=$id_usuario";
+        $resultado = $conexion_db->query($query);
+        $datos = [];
+        if($resultado->num_rows){
+            while($row = $resultado-> fetch_assoc()){
+                $datos[]=[
+                    'Us_ID'=>$row['Us_ID'],
+                    'Us_Usuario'=>$row['Us_Usuario'],
+                    'Us_Nivel'=>$row['Us_Nivel'],
+                    'Us_Contra'=>$row['Us_Contra'],
+                    'Us_Descripcion'=>$row['Us_Descripcion'],
+                ];
+            }//end while
+            return $datos;
+        }//end if
+    }//end getWhere
 }
 ?>
