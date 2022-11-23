bff3c1836c1aa86aff62fb9b11e397a26579171b
JesusGMgit <jesus.garcia.m1488@gmail.com>
2022-11-23 07:58:45 -0600

    prubas de json en post

--- backend/api/ar_tUsuarios.php
+++ backend/api/ar_tUsuarios.php
@@ -16,6 +16,8 @@ switch ($_SERVER['REQUEST_METHOD']) {
 
         case 'POST':
             echo "informacion: ". file_get_contents('php://input');
+            $_POST=json_decode(file_get_contents('php://input'));
+            echo " usuario: " . $_POST['Us_Usuario'];
             //$datos = json_decode(file_get_contents('php://input'));
             /*
             if($datos != NULL) {
