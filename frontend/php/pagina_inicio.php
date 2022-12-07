ddfcb605f6ea6aa3a023c11546374ffbce64110b
JesusGMgit <jesus.garcia.m1488@gmail.com>
2022-12-07 09:02:39 -0600

    pagina de inicio

--- frontend/php/pagina_inicio.php
+++ frontend/php/pagina_inicio.php
@@ -33,7 +33,7 @@ if(!isset($_SESSION['S_usuario_conectado'])|| !isset($_SESSION['S_usuario'])){
    
     <div class="w3-modal-content w3-card-4 w3-animate-zoom" style="max-width:600px">
 
-      <form class="w3-container" action="/php/usuario_log.php" method="post">
+      <form class="w3-container" action="frontend/php/usuario_log.php" method="post">
         <div class="w3-section">
           <label><b>Usuario</b></label>
           <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Ingresa tu Usuario" name="usuario" required>
