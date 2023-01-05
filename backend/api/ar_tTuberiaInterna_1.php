c4afa8e0030720276162c9c33ec34db8fb4d031c
JesusGMgit <jesus.garcia.m1488@gmail.com>
2023-01-05 16:52:23 -0600

    cambios_para api internas

---
+++ backend/api/ar_tTuberiaInterna_1.php
@@ -0,0 +1,87 @@
+<?php
+
+require_once "../clases/clase_soldadura_interna.php";
+header("Content-Type: application/json");
+switch ($_SERVER['REQUEST_METHOD']) {
+
+    case 'POST':
+            
+        $_POST=json_decode(file_get_contents('php://input'),true);
+        echo json_encode($_POST);
+         
+        if($_POST != NULL) {
+            if(soldadura_interna::crear_registro_tuberia_interna("tuberia_soldadura_interna_1",$_POST['Tin_ID_tubo'],$_POST['Tin_No_tubo'],$_POST['Tin_No_placa'],
+                                        $_POST['Tin_ID_proyecto'],$_POST['Tin_Lote_alambre'],$_POST['Tin_Lote_fundente'],$_POST['Tin_FolioOperador']
+                                        ,$_POST['Tin_Fecha'],$_POST['Tin_Hora'],$_POST['Tin_hora_db'],$_POST['Tin_Archivos_excel']
+                                        ,$_POST['Tin_Observaciones'])){
+                    
+                http_response_code(200);
+            }//end if
+            else {
+                http_response_code(400);
+            }//end else
+        }//end if
+        else {
+            http_response_code(405);
+        }//end else
+            
+        break;
+
+    case 'GET':
+
+        if(isset($_GET['Tin_ID_tubo'])) {
+            //echo "ID de tubo: " . $_GET['Tin_ID_tubo'];
+            echo json_encode(soldadura_interna::Leer_tubo_interna("tuberia_soldadura_interna_1",$_GET['Tin_ID_tubo']));
+        }//end if
+        else {
+            //echo "TODOS LOS REGISTROS DE TUBERIA EXTERNA 34";
+            echo json_encode(soldadura_interna::Leer_tuberia_interna("tuberia_soldadura_externa_1"));
+        }//end else
+
+        break;
+
+    case 'PUT':
+
+        $_PUT=json_decode(file_get_contents('php://input'),true);
+        //echo json_encode($_PUT);
+        if($_PUT != NULL) {
+            if(soldadura_interna::actualizar_tubo_interna("tuberia_soldadura_interna_1",$_GET['Tin_ID_tubo'],$_PUT['Tin_No_tubo'],$_PUT['Tin_No_placa'],
+                                                          $_PUT['Tin_ID_proyecto'],$_PUT['Tin_Lote_alambre'],$_PUT['Tin_Lote_fundente'],
+                                                          $_PUT['Tin_FolioOperador'],$_PUT['Tin_Fecha'],$_PUT['Tin_Hora'],$_PUT['Tin_hora_db'],
+                                                          $_PUT['Tin_Archivos_excel'],$_PUT['Tin_Observaciones'])) {
+                http_response_code(200);
+            }//end if
+            else {
+                http_response_code(400);
+            }//end else
+        }//end if
+        else {
+            http_response_code(405);
+        }//end else
+
+        break;
+
+    case 'DELETE':
+
+        if(isset($_GET['Tin_ID_tubo'])){
+            if(soldadura_interna::borrar_tubo_interna("tuberia_soldadura_interna_1",$_GET['Tin_ID_tubo'])) {
+                http_response_code(200);
+            }//end if
+            else {
+                http_response_code(400);
+            }//end else
+        }//end if 
+        else {
+            http_response_code(405);
+        }//end else
+
+        break;
+
+    default:
+
+        http_response_code(405);
+
+        break;
+
+}//end while
+?>
---
+++ backend/clases/clase_soldadura_interna.php
@@ -0,0 +1,121 @@
+<?php
+ 
+require_once "../clases/clase_conexion.php";
+
+
+class soldadura_interna{
+    
+    
+
+    public static function crear_registro_tuberia_interna($tuberia_in123,$Tin_ID_tubo,$Tin_No_tubo,$Tin_No_placa,
+                                                          $Tin_ID_proyecto,$Tin_Lote_alambre,$Tin_Lote_fundente,
+                                                          $Tin_FolioOperador,$Tin_Fecha,$Tin_Hora,$Tin_hora_db,
+                                                          $Tin_Archivos_excel,$Tin_Observaciones) {
+        $conexion_db =new Conexion();
+        $query = "INSERT INTO " . $tuberia_in123 . " (Tin_ID_tubo,Tin_No_tubo,Tin_No_placa,Tin_ID_proyecto,Tin_Lote_alambre,
+                                                      Tin_Lote_fundente,Tin_FolioOperador,Tin_Fecha,Tin_Hora,Tin_hora_db,
+                                                      Tin_Archivos_excel,Tin_Observaciones)
+        VALUES ('$Tin_ID_tubo', '$Tin_No_tubo', '$Tin_No_placa', '$Tin_ID_proyecto','$Tin_Lote_alambre',
+                '$Tin_Lote_fundente','$Tin_FolioOperador','$Tin_Fecha','$Tin_Hora','$Tin_hora_db','$Tin_Archivos_excel',
+                '$Tin_Observaciones')";
+        $conexion_db->query($query);
+        if($conexion_db->affected_rows) {
+            return TRUE;
+        }//end if
+        return FALSE;
+    }//end create_usuario
+
+    //Funciones por busqueda para de registros
+
+    public static function Leer_registros_interna_proyecto($tuberia_in123,$ID_proyecto){
+        
+    }
+
+    public static function Leer_tuberia_interna($tuberia_in123){
+        $conexion_db =new Conexion();
+        $query = "SELECT *FROM ". $tuberia_in123;
+        //echo "request: " . $query;
+        
+        $resultado = $conexion_db->query($query);
+        
+        $datos = [];
+        if($resultado->num_rows){
+            while($row = $resultado-> fetch_assoc()){
+                $datos[]=[
+                    'Tin_ID_tubo'=>$row['Tin_ID_tubo'],
+                    'Tin_No_tubo'=>$row['Tin_No_tubo'],
+                    'Tin_No_placa'=>$row['Tin_No_placa'],
+                    'Tin_ID_proyecto'=>$row['Tin_ID_proyecto'],
+                    'Tin_Lote_alambre'=>$row['Tin_Lote_alambre'],
+                    'Tin_Lote_fundente'=>$row['Tin_Lote_fundente'],
+                    'Tin_FolioOperador'=>$row['Tin_FolioOperador'],
+                    'Tin_Fecha'=>$row['Tin_Fecha'],
+                    'Tin_Hora'=>$row['Tin_Hora'],
+                    'Tin_Hora_db'=>$row['Tin_Hora_db'],
+                    'Tin_Archivos_excel'=>$row['Tin_Archivos_excel'],
+                    'Tin_Observaciones'=>$row['Tin_Observaciones']
+                ];
+                
+            }//end while
+            return $datos;
+            
+        }//end if
+        
+    }//end read_usuarios
+    
+    public static function Leer_tubo_interna($tuberia_in123,$ID_tubo) {
+        $conexion_db =new Conexion();
+        $query = "SELECT *FROM ".  $tuberia_in123." WHERE Tin_ID_tubo=$ID_tubo";
+        $resultado = $conexion_db->query($query);
+        $datos = [];
+        if($resultado->num_rows){
+            while($row = $resultado-> fetch_assoc()){
+                $datos[]=[
+                    'Tin_ID_tubo'=>$row['Tin_ID_tubo'],
+                    'Tin_No_tubo'=>$row['Tin_No_tubo'],
+                    'Tin_No_placa'=>$row['Tin_No_placa'],
+                    'Tin_ID_proyecto'=>$row['Tin_ID_proyecto'],
+                    'Tin_Lote_alambre'=>$row['Tin_Lote_alambre'],
+                    'Tin_Lote_fundente'=>$row['Tin_Lote_fundente'],
+                    'Tin_FolioOperador'=>$row['Tin_FolioOperador'],
+                    'Tin_Fecha'=>$row['Tin_Fecha'],
+                    'Tin_Hora'=>$row['Tin_Hora'],
+                    'Tin_Hora_db'=>$row['Tin_Hora_db'],
+                    'Tin_Archivos_excel'=>$row['Tin_Archivos_excel'],
+                    'Tin_Observaciones'=>$row['Tin_Observaciones']
+                ];
+            }//end while
+            return $datos;
+        }//end if
+    }//end read_usuario
+
+    public static function actualizar_tubo_interna($tuberia_in123,$Tin_ID_tubo,$Tin_No_tubo,$Tin_No_placa,
+                                               $Tin_ID_proyecto,$Tin_Lote_alambre,$Tin_Lote_fundente,
+                                               $Tin_FolioOperador,$Tin_Fecha,$Tin_Hora,$Tin_Hora_db,
+                                               $Tin_Archivos_excel,$Tin_Observaciones) {
+        $conexion_db = new Conexion();
+        $query = "UPDATE " .$tuberia_in123. " SET 
+                    Tin_No_tubo='" . $Tin_No_tubo . "', Tin_No_placa='" . $Tin_No_placa . 
+                    "', Tin_ID_proyecto='" . $Tin_ID_proyecto . "', Tin_Lote_alambre='" . $Tin_Lote_alambre . "', Tin_Lote_fundente='" . $Tin_Lote_fundente .
+                    "', Tin_FolioOperador='" . $Tin_FolioOperador . "', Tin_Fecha='" . $Tin_Fecha . "', Tin_Hora='".$Tin_Hora .
+                    "', Tin_Hora_db='" . $Tin_Hora_db . "', Tin_Archivos_excel='" . $Tin_Archivos_excel . "', Tin_Observaciones='" . $Tin_Observaciones .
+                    "' WHERE Tin_ID_tubo=" . $Tin_ID_tubo;
+        $conexion_db->query($query);
+        if($conexion_db->affected_rows) {
+            return TRUE;
+        }//end if
+        return FALSE;
+    }//end update_usuario
+
+    public static function borrar_tubo_interna($tuberia_in123,$ID_tubo) {
+        $conexion_db = new Conexion();
+        $query = "DELETE FROM " . $tuberia_in123 . " WHERE Pro_ID=$ID_tubo";
+        $conexion_db->query($query);
+        if($conexion_db->affected_rows) {
+            return TRUE;
+        }//end if
+        return FALSE;
+    }//end delete_usuario
+
+}
+?>
